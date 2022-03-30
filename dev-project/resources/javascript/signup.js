7d0d23485f72c68d85c3340f931e4bd5357f2c36
KaigaMunene <kaigamunene@gmail.com>
2022-03-30 05:16:28 +0300

    ft(project design): signup
    * create a function to link signup form with the server

--- dev-project/resources/javascript/signup.js
+++ dev-project/resources/javascript/signup.js
@@ -1,70 +1,111 @@
 function validateSignupForm(event) {
-    event.preventDefault();
-    const firstName = document.getElementById("first_name").value;
-    const lastName = document.getElementById("last_name").value;
-    const userName = document.getElementById("username").value;
-    const mail = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  event.preventDefault();
+  const firstName = document.getElementById("first_name").value;
+  const lastName = document.getElementById("last_name").value;
+  const userName = document.getElementById("username").value;
+  const mail = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    if (firstNameValidation(firstName) == "" || lastNameValidation(lastName) == "" || usernameValidation(userName) == "" || emailValidation(mail) == "" || passswordValidation(password) == "") {
-        return false;
-    }
+  let isValid = true;
+  if (!firstNameValidation(firstName)) {
+    document.getElementById("firstname-err").innerText =
+      "Enter a valid first name, must be one word";
+    isValid = false;
+  }
 
-    else {
-        alert("successfully signed up");
-        return true;
+  if (!lastNameValidation(lastName)) {
+    document.getElementById("lastname-err").innerText =
+      "Enter a valid last name, must be one word";
+    isValid = false;
+  }
+  if (!usernameValidation(userName)) {
+    document.getElementById("username-err").innerText =
+      "Enter a valid username,shouldn't be less than 6 characters";
+    isValid = false;
+  }
+  if (!emailValidation(mail)) {
+    document.getElementById("email-err").innerText =
+      "enter a valid email address";
+    isValid = false;
+  }
+  if (!passswordValidation(password)) {
+    document.getElementById("password-err").innerText =
+      "password should be more than 8 characters";
+    isValid = false;
+  }
+  if(isValid){
+    const formData = {
+      first_name: firstName,
+      last_name: lastName,
+      username: userName,
+      email: mail,
+      password: password
     }
+    signup(formData)
+  }
 }
 
 function firstNameValidation(firstName) {
-    let letters = /^[a-z ,.'-]+$/i
-    if (firstName.match(letters)) {
-        return true;
-    }
-    else {
-        document.getElementById("firstname-err").innerText = "Enter a valid first name, must be one word"
-        return false;
-    }
+  let letters = /^[a-z ,.'-]+$/i;
+  if (firstName.match(letters)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function lastNameValidation(lastName) {
-    let letters = /^[a-z ,.'-]+$/i
-    if (lastName.match(letters)) {
-        return true;
-    }
-    else {
-        document.getElementById("lastname-err").innerText = "Enter a valid last name, must be one word"
-        return false;
-    }
+  let letters = /^[a-z ,.'-]+$/i;
+  if (lastName.match(letters)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function usernameValidation(userName) {
-    let name = /^[a-z\d_]{6,16}$/
-    if (userName.match(name)) {
-        return true;
-    }
-    else {
-        document.getElementById("username-err").innerText = "Enter a valid username,shouldn't be less than 6 characters"
-        return false;
-    }
+  let name = /^[a-z\d_]{6,16}$/;
+  if (userName.match(name)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function emailValidation(mail) {
-    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (mail.match(mailformat)) {
-        return true;
-    }
-    else {
-        document.getElementById("email-err").innerText = "enter a valid email address"
-        return false;
-    }
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (mail.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function passswordValidation(password) {
-    let passwordLen = password.length
-    if (passwordLen == 0 || passwordLen < 8) {
-        document.getElementById("password-err").innerText = "password should be more than 8 characters"
-        return false;
-    }
-    return true;
+  let passwordLen = password.length;
+  if (passwordLen == 0 || passwordLen < 8) {
+    return false;
+  }
+  return true;
+}
+
+async function signup(formData) {
+  let data = JSON.stringify(formData);
+
+  try {
+    const register = await fetch(
+      "http://127.0.0.1:8000/api/v1/users/register/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      }
+    );
+    const response = await register.json();
+    console.log("Success:", response);
+  } catch (error) {
+    console.log("Error:", error);
+  }
 }
