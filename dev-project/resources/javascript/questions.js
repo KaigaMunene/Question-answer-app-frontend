83fcad3ac9cf06a80f2b2cc889c2311b44b42aa2
KaigaMunene <kaigamunene@gmail.com>
2022-04-12 11:08:30 +0300

    feature(project design) : modify the questions page
    * create a new function for append questions
    * modify the design

--- dev-project/resources/javascript/questions.js
+++ dev-project/resources/javascript/questions.js
@@ -2,29 +2,47 @@ import "regenerator-runtime/runtime";
 import axios from "axios";
 import { appBaseUrl } from "../utils/constants";
 
+const cardTemplate = document.getElementById("card-template");
+const questionGrid = document.getElementById("question-grid");
+
 getQuestions();
 
 async function getQuestions() {
   try {
-    let response = await axios({
+    const response = await axios({
       method: "GET",
       baseURL: appBaseUrl,
       url: `/qs/questions`,
     });
+    console.log(response);
     appendQuestions(response.data);
   } catch (error) {
     console.log(error.message);
   }
 }
 
-function appendQuestions(questions) {
+ /* function appendQuestions(questions) {
   let inquiry = "";
   for (let i = 0; i < questions.length; i++)
     inquiry += `
   <div>
   <a href="question.html?id=${questions[i].id}">
   <h1>${questions[i].title}</h1>
-  </a> 
+  </a>
   </div>`;
   document.getElementById("questions").innerHTML = inquiry;
-}
+} */
+
+const appendQuestions = (questions) => {
+  questions.forEach((question) => {
+    const questionCard = cardTemplate.cloneNode(true);
+    questionCard.querySelector("[data-title]").textContent = question.title;
+    questionCard.querySelector("[data-question]").textContent =
+      question.question;
+    questionGrid.append(questionCard);
+    questionCard.addEventListener("click", () => {
+      window.location = `answers.html?id=${question.id}`;
+    });
+  });
+
+};
